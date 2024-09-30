384234f37e51a4f965e996b638539f7fa08c6d89
finesse-21 <21pepperforreal@gmail.com>
2024-09-30 22:56:40 +0300

    Create button to enter number of rows

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const container = document.querySelector("#container");
-for (let i = 0; i < 256; i++) {
-    let block = document.createElement("div");
-    block.classList.add("block");
-    container.append(block);
+
+function createGrid(number) {
+    let container = document.querySelector("#container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for (let i = 0; i < number * number; i++) {
+        let block = document.createElement("div");
+        block.classList.add("block");
+        block.style.cssText = `height: calc(100%/${number}); width: calc(100%/${number})`
+        container.append(block);
+    }
 }
+const button = document.querySelector(".change");
+button.addEventListener("click", () => {
+    let number = prompt("Type a number of squares per side");
+    if (number > 100) alert("Type number not more than 100");
+    else createGrid(number);
+  });
