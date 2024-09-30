e938eb7e3c85f950301cfd70335fe2b60a974925
finesse-21 <21pepperforreal@gmail.com>
2024-09-30 22:18:47 +0300

    Create simple grid with hover

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+for (let i = 0; i < 256; i++) {
+    let block = document.createElement("div");
+    block.classList.add("block");
+    container.append(block);
+}
